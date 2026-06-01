137a8096afc771e788482f887fcef04ff0253f10
Gabrielius Butkus <personal.gabrielius@gmail.com>
2026-06-01 17:48:22 +0300

    feat: add loading skeleton for weather data

---
+++ client/src/components/WeatherSkeleton.jsx
@@ -0,0 +1,25 @@
+import { Column, Grid, SkeletonPlaceholder } from "@carbon/react";
+
+export default function WeatherSkeleton() {
+  return (
+    <>
+      <Grid>
+        <Column sm={4} md={4} lg={8} className="hero-column">
+          <SkeletonPlaceholder className="skeleton-conditions" />
+        </Column>
+        <Column sm={4} md={4} lg={8}>
+          <SkeletonPlaceholder className="skeleton-conditions" />
+        </Column>
+      </Grid>
+      <Grid>
+        <Column sm={4} md={8} lg={16}>
+          <div className="forecast-grid">
+            {Array.from({ length: 5 }).map((_, i) => (
+              <SkeletonPlaceholder key={i} className="skeleton-forecast" />
+            ))}
+          </div>
+        </Column>
+      </Grid>
+    </>
+  );
+}
